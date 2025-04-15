6c2771205f08c8159338de6e933b42265f15c096
Nicholas Tardie <ntardie@bucs.fsw.edu>
2025-04-15 09:54:50 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def convert():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port)
