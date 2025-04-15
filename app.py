e0c8b50c2331c0b7da7e9ae4c4c05ed31bd1ff3f
Nicholas Tardie <ntardie@bucs.fsw.edu>
2025-04-15 09:31:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -82,4 +83,5 @@ def convert():
     return jsonify({'result': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
